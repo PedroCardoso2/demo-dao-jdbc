32e059fbcd78c8003fdf1c0cc5bfbef356b9592f
Pedro Cardoso <www.pedrolucascardoso@gmail.com>
2023-07-03 16:58:17 -0300

    Implments FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		
-		Department dp = new Department(1,"Books");
-		
-		Seller seller = new Seller(10, "pedro", "pedrocardoso@gmail.com", new Date(),100.0, dp);
-		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
-		System.out.println(dp);
-		
 		
 	}
 }
